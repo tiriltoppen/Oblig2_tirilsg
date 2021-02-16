b4bd602c74871569ac394d853bd91d9086812f48
Tiril Gunnuldsen <32484555+tiriltoppen@users.noreply.github.com>
2021-02-16 18:47:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ try {
 		}
 		
 		// Use object destructuring to change the 3 statements below into 1 statement.
-		let {name, age, add} = obj
+		let {name, age, add} = obj;
 
 		// Don't make changes below this line	
 		
